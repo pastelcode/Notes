3aa84a6e4b151681dd47ecd6f5caf22f42e5ef2b
Pastel <pastel.dev.gt@gmail.com>
2023-12-21 22:20:38 -0600

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Samuel Marroquín on 12/21/23.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
